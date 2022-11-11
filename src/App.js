37b0a9d055070c3c77da4003476e08ef0db78141
Jefferson Choi Bernard <jefferson_bernard@brown.edu>
2022-11-11 17:27:00 -0500

    done

--- src/App.js
+++ src/App.js
@@ -11,20 +11,44 @@ bakeryData.forEach((item) => {
 function App() {
   // TODO: use useState to create a state variable to hold the state of the cart
   /* add your cart state code here */
+  const [cart, setCart] = useState([]);
+  const [total, setTotal] = useState(0);
+
+  const addToCart = (item) => {
+    setTotal(item.price + total);
+    cart.push(item);
+    //setCart(cart.append(item));
+  }
 
   return (
     <div className="App">
-      <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
+      <h1 style={{textAlign: 'center'}}>My Okay Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
 
-      {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
-        <p>Bakery Item {index}</p> // replace with BakeryItem component
-      ))}
+      <div className="cardsdiv">{bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
+        <div className="cardview">
+          <button onClick={() => addToCart(item)} className="plusbtn">+</button>
+          <img src={item.image} className="img" />
+          <p className="name">{item.name}</p>
+          <p className="description">{item.description}</p>
+          <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between', width: '18vw'}}>
+            <p className="price">{item.price}</p>
+          </div>
+        </div>
+      ))}</div>
 
-      <div>
         <h2>Cart</h2>
-        {/* TODO: render a list of items in the cart */}
+        <p className="total">Cart Total is ${total}</p>
+        <div className="cardsdiv">{cart.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
+        <div className="cardview">
+          <img src={item.image} className="img" />
+          <p className="name">{item.name}</p>
+          <p className="description">{item.description}</p>
+          <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between', width: '18vw'}}>
+            <p className="price">{item.price}</p>
+          </div>
+        </div>
+      ))}</div>
       </div>
-    </div>
   );
 }
 
